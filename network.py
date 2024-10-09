7ac570b0d52343b4ca37786bf5fd90c4deb5bbb6
nulljcd <nulljcd@gmail.com>
2024-10-08 21:40:51 -0400

    Update network.py

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import math
 import random
+import matplotlib.pyplot as plt
 
 class Activation:
   class ReLu:
@@ -31,14 +32,6 @@ class Activation:
       t = self.compute(z)
       return [1 - t[i] * t[i] for i in range(len(z))]
 
-  class Sigmoid:
-    def compute(self, z):
-      return [1 / (1 + math.exp(-z[i])) for i in range(len(z))]
-
-    def derivative(self, z):
-      a = [1 / (1 + math.exp(-z[i])) for i in range(len(z))]
-      return [a[i] * (1 - a[i]) for i in range(len(z))]
-
 class Loss:
   class MeanSquaredError:
     def compute(self, p, e):
@@ -51,6 +44,9 @@ class Loss:
     def derivative(self, p, e):
       return [p[i] - e[i] for i in range(len(p))]
 
+    class CrossEntropy:
+      pass
+
 class Network:
   def __init__(self, layerSizes, activation):
     self.layerSizes = layerSizes
@@ -58,8 +54,8 @@ class Network:
     self.numLayers = len(self.layerSizes)
     self.weights = [[[random.normalvariate(0, 1) for k in range(self.layerSizes[l])] for j in range(self.layerSizes[l - 1])] for l in range(1, self.numLayers)]
     self.biases = [[0 for j in range(self.layerSizes[l])] for l in range(0, self.numLayers)]
-    self.lossGradientW = [[[0 for k in range(self.layerSizes[l])] for j in range(self.layerSizes[l - 1])] for l in range(1, self.numLayers)]
-    self.lossGradientB = [[0 for j in range(self.layerSizes[l])] for l in range(0, self.numLayers)]
+    self.nablaW = [[[0 for k in range(self.layerSizes[l])] for j in range(self.layerSizes[l - 1])] for l in range(1, self.numLayers)]
+    self.nablaB = [[0 for j in range(self.layerSizes[l])] for l in range(0, self.numLayers)]
 
   def feedForward(self, x):
     for l in range(1, self.numLayers):
@@ -86,36 +82,36 @@ class Network:
       zs.append(z)
       activations.append(x)
 
-    error = [[0 for j in range(self.layerSizes[l])] for l in range(0, self.numLayers)]
+    delta = [[0 for j in range(self.layerSizes[l])] for l in range(0, self.numLayers)]
 
     dLoss = loss.derivative(activations[self.numLayers - 1], y)
     sp = self.activation.derivative(zs[self.numLayers - 1])
-    error[self.numLayers - 1] = [dLoss[i] * sp[i] for i in range(self.layerSizes[self.numLayers - 1])]
+    delta[self.numLayers - 1] = [dLoss[i] * sp[i] for i in range(self.layerSizes[self.numLayers - 1])]
 
     for l in range(self.numLayers - 2, 0, -1):
       sum = [0 for i in range(self.layerSizes[l])]
       for j in range(self.layerSizes[l]):
         for k in range(self.layerSizes[l + 1]):
-          sum[j] += self.weights[l][j][k] * error[l + 1][k]
+          sum[j] += self.weights[l][j][k] * delta[l + 1][k]
       sp = self.activation.derivative(zs[l])
-      error[l] = [sum[i] * sp[i] for i in range(self.layerSizes[l])]
+      delta[l] = [sum[i] * sp[i] for i in range(self.layerSizes[l])]
 
     for l in range(0, self.numLayers - 1):
       for j in range(self.layerSizes[l]):
         for k in range(self.layerSizes[l + 1]):
-          self.lossGradientW[l][j][k] = activations[l][j] * error[l + 1][k]
+          self.nablaW[l][j][k] = activations[l][j] * delta[l + 1][k]
     for l in range(0, self.numLayers):
       for j in range(self.layerSizes[l]):
-        self.lossGradientB[l][j] = error[l][j]
+        self.nablaB[l][j] = delta[l][j]
 
   def applyGradients(self, alpha):
     for l in range(0, self.numLayers - 1):
       for j in range(self.layerSizes[l]):
         for k in range(self.layerSizes[l + 1]):
-          self.weights[l][j][k] -= self.lossGradientW[l][j][k] * alpha
+          self.weights[l][j][k] -= self.nablaW[l][j][k] * alpha
     for l in range(0, self.numLayers):
       for j in range(self.layerSizes[l]):
-        self.biases[l][j] -= self.lossGradientB[l][j] * alpha
+        self.biases[l][j] -= self.nablaB[l][j] * alpha
 
   def getParams(self):
     params = []
@@ -140,3 +136,56 @@ class Network:
         self.biases[l][j] = params[i]
         i += 1
     return params
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x0 = [math.cos(i) * 0.8 + random.normalvariate(0, 0.1) for i in range(350)] + [random.normalvariate(0, 0.12) for i in range(50)]
+# y0 = [math.sin(i) * 0.8 + random.normalvariate(0, 0.1) for i in range(350)] + [random.normalvariate(0, 0.12) for i in range(50)]
+# x1 = [math.cos(i) * 0.45 + random.normalvariate(0, 0.08) for i in range(200)]
+# y1 = [math.sin(i) * 0.45 + random.normalvariate(0, 0.08) for i in range(200)]
+
+trainingDataCircle600pts = [[[math.cos(i) * 0.8 + random.normalvariate(0, 0.1), math.sin(i) * 0.8 + random.normalvariate(0, 0.1)], [-1]] for i in range(350)] + \
+  [[[random.normalvariate(0, 0.1), random.normalvariate(0, 0.1)], [-1]] for i in range(50)] + \
+  [[[math.cos(i) * 0.45 + random.normalvariate(0, 0.08), math.sin(i) * 0.45 + random.normalvariate(0, 0.08)], [1]] for i in range(200)]
+
+network = Network((2, 26, 1), Activation.TanH())
+loss = Loss.MeanSquaredError()
+
+for i in range(1000):
+  for j in range(600):
+    network.backPropagate(trainingDataCircle600pts[j][0], trainingDataCircle600pts[j][1], loss)
+    network.applyGradients(0.05)
+
+x0 = []
+y0 = []
+x1 = []
+y1 = []
+
+for i in range(40):
+  for j in range(40):
+    x = i / 20 - 1
+    y = j / 20 - 1
+    output = network.feedForward([x, y])[0]
+    if output < 0:
+      x0.append(x)
+      y0.append(y)
+    else:
+      x1.append(x)
+      y1.append(y)
+
+fig, ax = plt.subplots()
+ax.plot(x0, y0, '.')
+ax.plot(x1, y1, '.')
+ax.set(xlim=(-1, 1), ylim=(-1, 1))
+plt.show()
