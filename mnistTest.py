c76ef51c470dfad74dc7e18197eb1a29e3786414
nulljcd <nulljcd@gmail.com>
2024-12-09 18:51:28 -0800

    Update mnistTest.py

--- mnistTest.py
+++ mnistTest.py
@@ -1,9 +1,7 @@
 import numpy as np
 from ml import Activation, Loss, WeightInitializer, BiasInitializer, Model, Optimizer
 
-
 print("loading data")
-
 trainingData = []
 with open("mnist/train.txt", "r") as file:
   for line in file:
@@ -12,7 +10,6 @@ with open("mnist/train.txt", "r") as file:
     y = [0] * 10
     y[int(line[0])] = 1
     trainingData.append((x, y))
-
 testingData = []
 with open("mnist/test.txt", "r") as file:
   for line in file:
@@ -22,45 +19,36 @@ with open("mnist/test.txt", "r") as file:
     y[int(line[0])] = 1
     testingData.append((x, y))
 
-
 model = Model(
-  layerSizes=[784, 128, 10],
-  activation=Activation.ReLu(),
-  outputActivation=Activation.SoftMax(),
-  weightInitializer=WeightInitializer.He(),
-  biasInitializer=BiasInitializer.Zero())
-
+  [784, 128, 10],
+  Activation.ReLu(),
+  Activation.SoftMax(),
+  WeightInitializer.He(),
+  BiasInitializer.Constant(0.1))
 loss = Loss.CrossEntropy()
+optimizer = Optimizer.Adam(model, 0.0003, 0.9, 0.999, 1e-8, 0.0001)
 
-optimizer = Optimizer.Adam(
-  model=model,
-  eta=0.0003,
-  beta1=0.9,
-  beta2=0.999,
-  epsilon=1e-8,
-  weightDecay=0.0001)
-
+numEpochs = 5
+batchSize = 32
 
 print("training")
-
 model.initialize()
-
-for epoch in range(5):
-  print(f"  epoch: {epoch + 1}/5")
+for epoch in range(numEpochs):
+  print(f" epoch: {epoch + 1}/{numEpochs}")
   np.random.shuffle(trainingData)
-  for i in range(len(trainingData)):
-    model.backPropagate(trainingData[i][0], trainingData[i][1], loss=loss)
+  for i in range(batchSize, len(trainingData), batchSize):
+    batch = trainingData[i - batchSize : i]
+    for j in range(0, batchSize):
+      model.backPropagate(batch[j][0], batch[j][1], loss)
     optimizer.step()
     model.zeroGradients()
 
-
 print("evaluating")
-score = 0
+numCorrectTraining = 0
+numCorrectTesting = 0
 for i in range(len(trainingData)):
-  score += int(np.argmax(model.feedForward(trainingData[i][0])) == np.argmax(trainingData[i][1]))
-print(f"training score: {round(score / len(trainingData) * 100, 2)}%")
-
-score = 0
+  numCorrectTraining += int(np.argmax(model.feedForward(trainingData[i][0])) == np.argmax(trainingData[i][1]))
 for i in range(len(testingData)):
-  score += int(np.argmax(model.feedForward(testingData[i][0])) == np.argmax(testingData[i][1]))
-print(f"testing score: {round(score / len(testingData) * 100, 2)}%")
+  numCorrectTesting += int(np.argmax(model.feedForward(testingData[i][0])) == np.argmax(testingData[i][1]))
+print(f"training accuracy: {round(numCorrectTraining / len(trainingData) * 100, 2)}%")
+print(f"testing accuracy: {round(numCorrectTesting / len(testingData) * 100, 2)}%")
